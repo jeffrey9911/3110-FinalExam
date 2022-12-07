18f4c2a171c2b74803301a089790ce0fab19c5b1
Yaojie Li <jeffrey.yaojie.li@gmail.com>
2022-12-07 14:57:26 -0500

    Profiler

--- Assets/Scripts/ObjectPooling/EnemySpawner.cs
+++ Assets/Scripts/ObjectPooling/EnemySpawner.cs
@@ -10,30 +10,61 @@ public class EnemySpawner : MonoBehaviour
 
     [SerializeField] private int _NumberOfEnemy;
 
+
+    public GameObject _E1;
+    public GameObject _E2;
+
+    public bool isUsingOBJP;
+
     private int _MaxNumber;
 
 
     private void Start()
     {
         _MaxNumber = ObjectPoolManager.instance._objectPoolDictionary["Enemy1"].Count;
-        for (int i = 0; i < _NumberOfEnemy; i++)
+        if(isUsingOBJP)
         {
-            float select = Random.Range(0f, 10f);
-            if (((int)select % 2) == 0)
+            for (int i = 0; i < _NumberOfEnemy; i++)
             {
-                ObjectPoolManager.instance.SetActiveFromPool("Enemy1", _SpawnPos1.position, Quaternion.identity);
-            }
-            else
-            {
-                ObjectPoolManager.instance.SetActiveFromPool("Enemy2", _SpawnPos2.position, Quaternion.identity);
-            }
+                float select = Random.Range(0f, 10f);
+                if (((int)select % 2) == 0)
+                {
+                    ObjectPoolManager.instance.SetActiveFromPool("Enemy1", _SpawnPos1.position, Quaternion.identity);
+                }
+                else
+                {
+                    ObjectPoolManager.instance.SetActiveFromPool("Enemy2", _SpawnPos2.position, Quaternion.identity);
+                }
 
 
-            if (i == _MaxNumber)
-            {
-                break;
+                if (i == _MaxNumber)
+                {
+                    break;
+                }
             }
         }
+        else
+        {
+            for (int i = 0; i < _NumberOfEnemy; i++)
+            {
+                float select = Random.Range(0f, 10f);
+                if (((int)select % 2) == 0)
+                {
+                    GameObject gobj = Instantiate(_E1, _SpawnPos1.position, Quaternion.identity);
+                }
+                else
+                {
+                    GameObject gobj = Instantiate(_E1, _SpawnPos1.position, Quaternion.identity);
+                }
+
+
+                if (i == _MaxNumber)
+                {
+                    break;
+                }
+            }
+        }
+        
     }
 
     private void Update()
